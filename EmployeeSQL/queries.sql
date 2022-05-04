71648b0d4ffe79f589dc3340584a23d59d8da505
egrove <ellen.grove95@gmail.com>
2022-05-04 17:20:47 -0500

    finish writing queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,7 +1,7 @@
 -- List the following details of each employee: 
 -- employee number, last name, first name, sex, and salary.
-SELECT emp_no, last_name, first_name, sex, 
-	(SELECT salary
+SELECT emp_no, last_name, first_name, sex, (
+	SELECT salary
 	FROM salaries
 	WHERE employees.emp_no = salaries.emp_no)
 FROM employees;
@@ -19,14 +19,21 @@ WHERE
 -- with the following information: department number, 
 -- department name, the manager's employee number, 
 -- last name, first name.
+SELECT m.dept_no, d.dept_name, m.emp_no, e.first_name, e.last_name
+FROM dept_manager m
+LEFT JOIN departments d ON
+	m.dept_no = d.dept_no
+	LEFT JOIN employees e ON
+		m.emp_no = e.emp_no
+;
 
 
 -- List the department of each employee 
 -- with the following information: employee number, 
 -- last name, first name, and department name.
 -- REWORK
-SELECT emp_no, last_name, first_name,
-	(SELECT d.dept_name
+SELECT emp_no, last_name, first_name,(
+	SELECT d.dept_name
 	FROM dept_emp de
 	LEFT JOIN departments d ON de.dept_no = d.dept_no
 	WHERE employees.emp_no = de.emp_no)
@@ -45,12 +52,48 @@ WHERE
 -- List all employees in the Sales department, 
 -- including their employee number, last name, 
 -- first name, and department name.
+CREATE VIEW sales As
+	SELECT e.emp_no, e.dept_no, d.dept_name
+	FROM dept_emp As e
+	LEFT JOIN departments As d
+		ON e.dept_no = d.dept_no
+	WHERE 
+		d.dept_name = 'Sales'
+;
 
+SELECT emp_no, last_name, first_name, (
+	SELECT dept_name
+	FROM sales
+	WHERE employees.emp_no = sales.emp_no)
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM sales)
+;
 
 -- List all employees in the Sales and Development departments,
 -- including their employee number, last name, first name,
 -- and department name.
+CREATE VIEW sales_dev As
+	SELECT e.emp_no, e.dept_no, d.dept_name
+	FROM dept_emp As e
+	LEFT JOIN departments As d
+		ON e.dept_no = d.dept_no
+	WHERE 
+		d.dept_name = 'Development' OR
+		d.dept_name = 'Sales'
+;
 
+SELECT emp_no, last_name, first_name, (
+	SELECT dept_name
+	FROM sales_dev
+	WHERE employees.emp_no = sales_dev.emp_no
+	)
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM sales_dev)
+;
 
 -- List the frequency count of employee last names 
 -- (i.e., how many employees share each last name) 
