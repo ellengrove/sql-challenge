6050648c41ae44b355bc5dcb206c9a1b7b4ba2d7
egrove <ellen.grove95@gmail.com>
2022-05-05 11:49:09 -0500

    updated query summarizing department by employee

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -31,14 +31,13 @@ LEFT JOIN departments d ON
 -- List the department of each employee 
 -- with the following information: employee number, 
 -- last name, first name, and department name.
--- REWORK
-SELECT emp_no, last_name, first_name,(
-	SELECT d.dept_name
-	FROM dept_emp de
-	LEFT JOIN departments d ON de.dept_no = d.dept_no
-	WHERE employees.emp_no = de.emp_no)
-FROM employees;
-	
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+LEFT JOIN departments d ON
+	de.dept_no = d.dept_no
+	LEFT JOIN employees e ON
+		de.emp_no = e.emp_no;
+
 	
 -- List first name, last name, and sex for employees
 -- whose first name is "Hercules" and last names begin with "B."
